e9bc48432c5c327c21d66e14cf19c0ae1ef7b019
asher <2651016635@qq.com>
2024-01-19 04:49:16 +0000

    [streaming-latestinvoices](Step 1) move  data fetches down to the components that need it

--- nextjs-dashboard/app/ui/dashboard/latest-invoices.tsx
+++ nextjs-dashboard/app/ui/dashboard/latest-invoices.tsx
@@ -2,12 +2,11 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+// import { LatestInvoice } from '@/app/lib/definitions'; 
+import { fetchLatestInvoices } from '@/app/lib/data';
+export default async function LatestInvoices() {//[streaming-latestinvoices](Step 1) remove the LatestInvoice type param which is from dashboard page
+  //[streaming-latestinvoices](Step 1) move  data fetches down to the components that need it
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
