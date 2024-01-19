0da8d10ad8f26d2944036715cd1a8bde32932614
asher <2651016635@qq.com>
2024-01-19 13:57:13 +0000

    [Insert data]: Create insert SQL query

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,7 +1,8 @@
 'use server';
 
 import { z } from 'zod';
- 
+import { sql } from '@vercel/postgres';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -14,10 +15,18 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
 
-  const { customerId, amount, status }  = CreateInvoice.parse({
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
 
 }
