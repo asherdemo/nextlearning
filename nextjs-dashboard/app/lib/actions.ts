818031faf2eb435b6b7fc14fc1c25c9fb36f687c
asher <2651016635@qq.com>
2024-01-20 06:50:01 +0000

    [Delete Invoice]: wrap button with form && pass id param and bind to deleteInvoice

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -54,3 +54,8 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+
+export async function deleteInvoice(id: string){
+
+}
